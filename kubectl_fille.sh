5c4dc58b2f004f616579453bb6d3d97a648c2ffd
Ikrupals <krupal.suthar@Softnautics.com>
2024-08-02 16:51:25 +0530

    add kubectl file

---
+++ kubectl_fille.sh
@@ -0,0 +1,58 @@
+kubectl get nodes
+deploymentYaml=$(cat <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: fastapi-deployment
+  labels:
+	app: fastapi
+spec:
+  replicas: 1
+  selector:
+	matchLabels:
+	  app: fastapi
+  template:
+	metadata:
+	  labels:
+		app: fastapi
+	spec:
+	  containers:
+	  - name: fastapi
+		image: $1/$2:$3
+		ports:
+		- containerPort: 8082
+EOF
+)
+
+
+serviceYaml=$(cat <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: fastapi-service
+spec:
+  selector:
+	app: fastapi
+  ports:
+	- protocol: TCP
+	  port: 8082
+	  targetPort: 8082
+  type: LoadBalancer
+EOF
+)
+if [ -e deployment.yml ]
+then
+	rm -rf deployment.yml
+fi
+if [ -e service.yml ]
+then
+	rm -rf service.yml
+fi
+
+echo "$deploymentYaml" > deployment.yml
+echo "$serviceYaml" > service.yml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+sleep 50
+kubectl destroy
+
